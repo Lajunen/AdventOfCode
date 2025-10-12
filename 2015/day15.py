c78dee323350c5be138c6ffc1dc043807b0de218
Sami Berg <samiberg78@hotmail.com>
2025-10-13 00:37:54 +0300

    year 2015, day 15 completed

--- 2015/day15.py
+++ 2015/day15.py
@@ -7,28 +7,17 @@ ready = 0
 def day15(p):
     data = readfile.read_lines(15)
 
-    ## ingredients = parse_data(data)
-    ingredients = []
-    new_ing = ingredient()
-    new_ing.name = "Butterschotch"
-    new_ing.capacity = -1
-    new_ing.durability = -2
-    new_ing.flavor = 6
-    new_ing.texture = 3
-    ingredients.append(new_ing)
-    second = ingredient()
-    second.name = "Cinnamon"
-    second.capacity = 2
-    second.durability = 3
-    second.flavor = -2
-    second.texture = -1
-    ingredients.append(second)
+    ingredients = parse_data(data)
 
     global high_score
 
     if p == 1:
-        create_cookie([], ingredients)
+        create_cookie([], ingredients, 0, p)
         print("Day 15, Part 1 solution: " + str(high_score))
+    
+    if p == 2:
+        create_cookie([], ingredients, 0, p)
+        print("Day 15, Part 2 solution: " + str(high_score))
 
 def parse_data(data):
     ingredients = []
@@ -50,76 +39,78 @@ def parse_data(data):
     
     return ingredients
 
-def create_cookie(recipe, ingredients):
+def create_cookie(recipe, ingredients, ingredient_number, p):
     amount = 0
     for part in recipe:
         amount += part[1]
     
-    ## print(str(recipe))
-    
-    if amount == 0:
-        new_recipe = []
-        ## first time entering, adding 1 of each ingredient to start with
-        for ing in ingredients:
-            teaspoon = [ing.name, 1]
-            new_recipe.append(teaspoon)
-
-        create_cookie(new_recipe, ingredients)
-        return
-    
-    if amount == 10:
-        print(recipe)
+    if amount == 100:
         ## recipe full, calculating score
-        score = 0
-
-        for ing in recipe:
-            for info in ingredients:
-                if ing[0] == info.name:
-                    ## adding points from capacity
-                    part_score = ing[1] * info.capacity
-                    if part_score < 0:
-                        part_score = 0
-                    score += part_score
-
-                    ## adding points from durability
-                    part_score = ing[1] * info.durability
-                    if part_score < 0:
-                        part_score = 0
-                    score += part_score
-
-                    ## adding points from flavor
-                    part_score = ing[1] * info.flavor
-                    if part_score < 0:
-                        part_score = 0
-                    score += part_score
-
-                    ## adding points from texture
-                    part_score = ing[1] * info.texture
-                    if part_score < 0:
-                        part_score = 0
-                    score += part_score
+        score = count_score(recipe, ingredients, p)
 
         global high_score
 
-        if high_score < score:
+        if high_score < score and score != 0:
             high_score = score
         
         return
     
-    if amount > 0 and amount < 10:
-        for ing in ingredients:
-            new_recipe = []
-            for part in recipe:
-                if part[0] == ing.name:
-                    new_amount = part[1] + 1
-                    new_teaspoon = [part[0], new_amount]
-                    new_recipe.append(new_teaspoon)
-                else:
-                    new_teaspoon = [part[0], part[1]]
-                    new_recipe.append(new_teaspoon)
-            create_cookie(new_recipe, ingredients)
+    if amount < 100:
+        if ingredient_number == len(ingredients) - 1:
+            ## last ingredient
+            new_recipe = add_to_recipe(recipe, [ingredients[ingredient_number].name, 100-amount])
+            create_cookie(new_recipe, ingredients, ingredient_number, p)
+        else:
+            ## ingredients left
+            for amo in range(0, 101 - amount):
+                ## loop to fill in this ingredient and moving to next
+                new_recipe = add_to_recipe(recipe, [ingredients[ingredient_number].name, amo])
+                create_cookie(new_recipe, ingredients, ingredient_number+1, p)
+
         return
-                                
+
+def count_score(recipe, ingredients, p):
+    part_capa = 0
+    part_dura = 0
+    part_flav = 0
+    part_text = 0
+    part_calo = 0
+
+    for ing in recipe:
+        for info in ingredients:
+            if ing[0] == info.name:
+                ## adding points of current ingredient
+                part_capa += ing[1] * int(info.capacity)
+                part_dura += ing[1] * int(info.durability)
+                part_flav += ing[1] * int(info.flavor)
+                part_text += ing[1] * int(info.texture)
+                part_calo += ing[1] * int(info.calories)
+
+    if part_capa <= 0 or part_dura <= 0 or part_flav <= 0 or part_text <= 0:
+        return 0
+    else:
+        if p == 1:
+            return part_capa * part_dura * part_flav * part_text
+        if p == 2:
+            if part_calo != 500:
+                return 0
+            else:
+                return part_capa * part_dura * part_flav * part_text
+
+def add_to_recipe(recipe, new_part):
+    new_recipe = []
+    part_found = False
+    for part in recipe:
+        if part[0] == new_part[0]:
+            new_recipe.append([part[0], part[1] + new_part[1]])
+            part_found = True
+        else:
+            new_recipe.append(part)
+    
+    if not part_found:
+        new_recipe.append(new_part)
+    
+    return new_recipe
 
 class ingredient:
     name = ""
@@ -129,4 +120,4 @@ class ingredient:
     texture = 0
     calories = 0
 
-day15(1)
+day15(2)
