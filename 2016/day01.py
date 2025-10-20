da777a3e3cd3649d21caf91655e989b489c7d878
Sami Berg <samiberg78@hotmail.com>
2025-10-20 09:30:13 +0300

    day 01 completed from year 2016

---
+++ 2016/day01.py
@@ -0,0 +1,71 @@
+import readfile
+from copy import deepcopy
+
+def day01(p):
+    route_raw = readfile.read_line(1)
+    route = parse_data(route_raw)
+
+    if p == 1:
+        print("Day 1, Part 1 solution: " + str(walk_route(route, 1)))
+    if p == 2:
+        print("Day 1, Part 2 solution: " + str(walk_route(route, 2)))
+
+def walk_route(route, part):
+    ## variables for both parts
+    heading = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    facing = 0
+    location = [0,0]
+
+    ## part two added variables
+    locations_visited = []
+    locations_visited.append(deepcopy(location))
+    part2_finished = False
+
+    for step in route:
+        if step[0] == "R":
+            facing += 1
+            if facing == 4:
+                facing = 0
+        elif step[0] == "L":
+            facing -= 1
+            if facing == -1:
+                facing = 3
+        for i in range(0, step[1], 1):
+            location[0] = location[0] + (1*heading[facing][0])
+            location[1] = location[1] + (1*heading[facing][1])
+            if part == 2:
+                if location in locations_visited:
+                    part2_finished = True
+                    break
+                else:
+                    locations_visited.append(deepcopy(location))
+        if part2_finished:
+            break
+
+
+    distance = 0
+    if location[0] < 0:
+        distance += location[0] * (-1)
+    else:
+        distance += location[0]
+    if location[1] < 0:
+        distance += location[1] * (-1)
+    else:
+        distance += location[1]
+
+    return distance  
+
+def parse_data(data):
+    data = data.replace(",", "")
+    
+    route = []
+    parts = data.split(" ")
+    for part in parts:
+        step = []
+        step.append(part[0:1])
+        step.append(int(part[1:]))
+        route.append(step)
+    
+    return route
+
+day01(2)
---
+++ 2016/main.py
@@ -0,0 +1,79 @@
+import day01
+import day02
+import day03
+import day04
+import day05
+import day06
+import day07
+import day08
+import day09
+import day10
+import day11
+import day12
+import day13
+import day14
+import day15
+import day16
+import day17
+import day18
+import day19
+import day20
+import day21
+import day22
+import day23
+import day24
+import day25
+
+def main(d, p):
+    if d == 1:
+        day01.day01(p)
+    if d == 2:
+        day02.day02(p)
+    if d == 3:
+        day03.day03(p)
+    if d == 4:
+        day04.day04(p)
+    if d == 5:
+        day05.day05(p)
+    if d == 6:
+        day06.day06(p)
+    if d == 7:
+        day07.day07(p)
+    if d == 8:
+        day08.day08(p)
+    if d == 9:
+        day09.day09(p)
+    if d == 10:
+        day10.day10(p)
+    if d == 11:
+        day11.day11(p)
+    if d == 12:
+        day12.day12(p)
+    if d == 13:
+        day13.day13(p)
+    if d == 14:
+        day14.day14(p)
+    if d == 15:
+        day15.day15(p)
+    if d == 16:
+        day16.day16(p)
+    if d == 17:
+        day17.day17(p)
+    if d == 18:
+        day18.day18(p)
+    if d == 19:
+        day19.day19(p)
+    if d == 20:
+        day20.day20(p)
+    if d == 21:
+        day21.day21(p)
+    if d == 22:
+        day22.day22(p)
+    if d == 23:
+        day23.day23(p)
+    if d == 24:
+        day24.day24(p)
+    if d == 25:
+        day25.day25(p)
+
+main(24, 1)
